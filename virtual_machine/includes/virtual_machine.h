caf6a4b6e619f632ba1fa96b581205dbb72b6b3f
Dav HOJT <davhojt@gmail.com>
2018-06-04 21:35:04 +0200

    Added structs to parse bytes.

--- virtual_machine/includes/virtual_machine.h
+++ virtual_machine/includes/virtual_machine.h
@@ -6,7 +6,7 @@
 /*   By: ffloris <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/27 20:58:52 by ffloris           #+#    #+#             */
-/*   Updated: 2018/05/27 20:59:15 by ffloris          ###   ########.fr       */
+/*   Updated: 2018/06/04 21:33:35 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,24 @@
 # define VIRTUAL_MACHINE_H
 
 # include "libft.h"
+
+typedef struct			s_byte_code
+{
+	int					byte;
+	struct s_byte_code	*next;
+}						t_byte_code;
+
+typedef struct			s_parser
+{
+	struct byte_code	*byte_code;
+	struct s_parser		*next;
+}						t_parser;
+
+typedef struct			s_vm
+{
+	char				**argv;
+	
+	struct s_parser		*parser;
+}						t_vm;
+
 #endif
