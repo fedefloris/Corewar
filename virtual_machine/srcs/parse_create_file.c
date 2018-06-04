3d93e7cef55e86219dad7ca5d53f3f5f4b04eea5
Dav HOJT <davhojt@gmail.com>
2018-06-05 00:56:10 +0200

    Added parse_create_file. Now creates one file per command line argument and saves command line argument as ->file_name

---
+++ virtual_machine/srcs/parse_create_file.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_create_file.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/04 23:00:00 by dhojt             #+#    #+#             */
+/*   Updated: 2018/06/05 00:51:43 by dhojt            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "virtual_machine.h"
+
+static t_file		*create_file(t_vm *vm)
+{
+	t_file			*file;
+
+	if (!(file = (t_file *)malloc(sizeof(t_file))))
+		error_exit(vm);
+	ft_memset(file, 0, sizeof(*file));
+	return (file);
+}
+
+void				parse_create_file(t_vm *vm)
+{
+	t_file			*file;
+
+	while (--vm->argc)
+	{
+		file = create_file(vm);
+		file->file_name = ft_strdup(vm->argv[vm->argc]);
+		if (!vm->file)
+			vm->file = file;
+		else
+		{
+			file->next = vm->file;
+			vm->file = file;
+		}
+	}
+}
