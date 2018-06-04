43e28d35259fd92256fdf76bae6c4e9f24ff992b
Dav HOJT <davhojt@gmail.com>
2018-06-04 21:36:47 +0200

    Added static function to create vm (virtual_machine struct).

--- virtual_machine/srcs/main.c
+++ virtual_machine/srcs/main.c
@@ -6,13 +6,31 @@
 /*   By: ffloris <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/27 20:35:56 by ffloris           #+#    #+#             */
-/*   Updated: 2018/05/27 21:01:15 by ffloris          ###   ########.fr       */
+/*   Updated: 2018/06/04 21:34:16 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "virtual_machine.h"
 
-int		main(void)
+static t_vm			*create_virtual_machine(char **argv)
 {
+	t_vm			*vm;
+
+	if(!(vm = (t_vm *)malloc(sizeof(t_vm))))
+		exit(1);
+	vm->parser = NULL;
+	vm->argv = argv;
+	return (vm);
+}
+
+int					main(int argc, char **argv)
+{
+	t_vm			*vm;
+
+	if (argc < 2)
+		ft_putstr("No args - CREATE\n");//Create usage
+	vm = create_virtual_machine(argv);
+	virtual_machine(vm);
+	//free fraeme here.
 	return (0);
 }
