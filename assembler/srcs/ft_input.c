d223b284cc37c46f0aaa501a66a96d8378a1ced6
Anastasios KASERIS <akaseris@e2r6p12.42.fr>
2018-06-06 15:49:27 +0200

    ft_input asm

--- assembler/srcs/ft_input.c
+++ assembler/srcs/ft_input.c
@@ -6,13 +6,36 @@
 /*   By: akaseris <akaseris@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/04 17:51:26 by akaseris          #+#    #+#             */
-/*   Updated: 2018/06/04 18:00:55 by akaseris         ###   ########.fr       */
+/*   Updated: 2018/06/06 15:48:57 by akaseris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "assembler.h"
 
-int				ft_input(int fd)
+int		ft_valid_line(char *s)
 {
-	
+	char	**split;
+
+	if (!s)
+		return (1);
+	split = ft_strsplit(s, '\t');
+
+}
+
+int		ft_input(int fd)
+{
+	char	*str;
+
+	while(get_next(fd, &str, '\n'))
+	{
+		if (!ft_valid_line(str))
+		{
+			ft_strdel(&str);
+			return (0);
+		}
+		ft_strdel(&str);
+	}
+	if (str)
+		ft_strdel(&str);
+	return ((str) ? 0 : 1);
 }
