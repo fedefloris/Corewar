266447392daeda055f8ce58ff75d24dbabfbe13a
Dav HOJT <davhojt@gmail.com>
2018-06-13 01:01:33 +0200

    Normed.

--- virtual_machine/srcs/decode_byte.c
+++ virtual_machine/srcs/decode_byte.c
@@ -6,24 +6,24 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/13 00:09:55 by dhojt             #+#    #+#             */
-/*   Updated: 2018/06/13 00:51:19 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/06/13 00:59:29 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "virtual_machine.h"
 
-char				decode_byte(t_vm *vm, unsigned char *encoded, int bit_pair)
+char				decode_byte(t_vm *vm, unsigned char *encoded, int pair)
 {
 	unsigned char	byte;
 
 	byte = *encoded;
-	if (bit_pair < 1 || bit_pair > 4)
+	if (pair < 1 || pair > 4)
 		error_exit(vm, "Error decoding encoded byte");
-	if (bit_pair == 2)
+	if (pair == 2)
 		byte <<= 2;
-	if (bit_pair == 3)
+	if (pair == 3)
 		byte <<= 4;
-	if (bit_pair == 4)
+	if (pair == 4)
 		byte <<= 6;
 	byte >>= 6;
 	return (byte);
