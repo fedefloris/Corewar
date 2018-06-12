8fb7a03d78803193be0f7b18e27b82e9b5dce53d
Dav HOJT <davhojt@gmail.com>
2018-06-12 19:19:42 +0200

    Removed debugging lines and normed.

--- virtual_machine/srcs/modify_pc.c
+++ virtual_machine/srcs/modify_pc.c
@@ -6,36 +6,21 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/12 18:26:09 by dhojt             #+#    #+#             */
-/*   Updated: 2018/06/12 19:09:12 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/06/12 19:19:21 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "virtual_machine.h"
-//
-//
-//
-#include <stdlib.h>
-#include <stdio.h>
-#define REG_NUMBER 15
-#define MEM_SIZE (4*1024)
-typedef struct			s_process
-{
-	int					number;
-	intmax_t			r[REG_NUMBER + 1];
-	int					pc;
-	char				carry;
-	struct s_process	*next;
-}						t_process;
+#include "virtual_machine.h"
 
 static void			modify_negative(t_process *process, int modify)
 {
 	int				pc;
 
 	pc = process->pc;
-	while(modify++)
+	while (modify++)
 	{
 		if (pc == 0)
-			pc = MEM_SIZE -1;
+			pc = MEM_SIZE - 1;
 		else
 			pc--;
 	}
@@ -47,7 +32,7 @@ static void			modify_positive(t_process *process, int modify)
 	int				pc;
 
 	pc = process->pc;
-	while(modify--)
+	while (modify--)
 	{
 		if (pc == MEM_SIZE - 1)
 			pc = 0;
@@ -63,15 +48,4 @@ void				modify_pc(t_process *process, int modify)
 		modify_positive(process, modify);
 	if (modify < 0)
 		modify_negative(process, modify);
-}
-
-//
-int					main(void)
-{
-	t_process				*process;
-	process = (t_process *)malloc(sizeof(t_process));
-	process->pc = 1267;
-	modify_pc(process, -60);
-	printf("%d\n", process->pc);
-	return (0);
 }
