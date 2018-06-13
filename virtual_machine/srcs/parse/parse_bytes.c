039e34d366c6f6e71358ffc0b1c3a257516b247a
Dav HOJT <davhojt@gmail.com>
2018-06-13 14:55:50 +0200

    Corrected Macro typo.

--- virtual_machine/srcs/parse/parse_bytes.c
+++ virtual_machine/srcs/parse/parse_bytes.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/11 12:25:33 by dhojt             #+#    #+#             */
-/*   Updated: 2018/06/13 09:26:49 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/06/13 14:55:14 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void				get_bytes(t_vm *vm, t_champ *champ)
 	i = 0;
 	bytes_len = 0;
 	byte_code = champ->byte_code;
-	while (byte_code && i++ < PROGRAM_NAME_LENGTH + COMMENT_LENGTH + 16)
+	while (byte_code && i++ < PROG_NAME_LENGTH + COMMENT_LENGTH + 16)
 		byte_code = byte_code->next;
 	if (!(champ->bytes = (char *)malloc(sizeof(char) * champ->prog_size)))
 		error_exit(vm, "Malloc failed (champ name)");
@@ -70,7 +70,7 @@ void				get_prog_size(t_vm *vm, t_champ *champ)
 
 	i = 0;
 	byte_code = champ->byte_code;
-	while (byte_code && i++ < PROGRRAM_NAME_LENGTH + 8)
+	while (byte_code && i++ < PROG_NAME_LENGTH + 8)
 		byte_code = byte_code->next;
 	i = 0;
 	while (byte_code && i++ < 4)
