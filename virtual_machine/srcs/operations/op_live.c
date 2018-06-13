2cfdc795c13636b60592c217f41fdf3028b68dad
Dav HOJT <davhojt@gmail.com>
2018-06-13 16:10:11 +0200

    Iterated pc once more on completion of live.

--- virtual_machine/srcs/operations/op_live.c
+++ virtual_machine/srcs/operations/op_live.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/06 23:03:26 by dhojt             #+#    #+#             */
-/*   Updated: 2018/06/13 15:01:06 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/06/13 16:09:51 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,4 +27,5 @@ void				op_live(t_vm *vm, t_process *ps)
 	}
 	vm->live_calls++;
 	vm->last_live = player_number;
+	modify_pc(ps, 1);
 }
