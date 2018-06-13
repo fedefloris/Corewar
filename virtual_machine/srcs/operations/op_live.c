2a147817dd29b861e51d5f7cd205d65b7295c417
Dav HOJT <davhojt@gmail.com>
2018-06-13 15:01:38 +0200

    Modifies live_calls and last_live

--- virtual_machine/srcs/operations/op_live.c
+++ virtual_machine/srcs/operations/op_live.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/06 23:03:26 by dhojt             #+#    #+#             */
-/*   Updated: 2018/06/13 14:58:12 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/06/13 15:01:06 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,6 @@ void				op_live(t_vm *vm, t_process *ps)
 		player_number <<= 8;
 		player_number += vm->memory[ps->pc];
 	}
+	vm->live_calls++;
+	vm->last_live = player_number;
 }
