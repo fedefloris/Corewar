3ca52663653b40037b8b1e232275f46d808ae364
Dav HOJT <davhojt@gmail.com>
2018-06-14 21:45:48 +0200

    encoded parameter is no longer a pointer.

--- virtual_machine/srcs/decode_byte.c
+++ virtual_machine/srcs/decode_byte.c
@@ -6,25 +6,22 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/13 00:09:55 by dhojt             #+#    #+#             */
-/*   Updated: 2018/06/13 00:59:29 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/06/14 21:42:12 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "virtual_machine.h"
 
-char				decode_byte(t_vm *vm, unsigned char *encoded, int pair)
+char				decode_byte(t_vm *vm, unsigned char encoded, int pair)
 {
-	unsigned char	byte;
-
-	byte = *encoded;
 	if (pair < 1 || pair > 4)
 		error_exit(vm, "Error decoding encoded byte");
 	if (pair == 2)
-		byte <<= 2;
+		encoded <<= 2;
 	if (pair == 3)
-		byte <<= 4;
+		encoded <<= 4;
 	if (pair == 4)
-		byte <<= 6;
-	byte >>= 6;
-	return (byte);
+		encoded <<= 6;
+	encoded >>= 6;
+	return (encoded);
 }
