872f566af0ce741dc00f4b6a12bc5866c1bcf3ca
Dav HOJT <davhojt@gmail.com>
2018-06-15 00:54:16 +0200

    Debugging and.

--- virtual_machine/srcs/operations/op_and.c
+++ virtual_machine/srcs/operations/op_and.c
@@ -6,26 +6,52 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/06 23:02:33 by dhojt             #+#    #+#             */
-/*   Updated: 2018/06/14 18:29:00 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/06/15 00:50:14 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "virtual_machine.h"
 
+void				get_value(t_vm *vm, t_process *ps, unsigned char encoded, int parameter, int pair)//make static or move
+{
+	int				value;
+
+	value = 0;
+	if (decode_byte(encoded, pair) == REG_CODE)
+		value = ps->r[parameter];
+	if (decode_byte(encoded, pair) == DIR_CODE)
+		;
+	if (decode_byte(encoded, pair) == IND_CODE)
+		value = vm->memory[ps->pc + parameter];
+}
+
 void				op_and(t_vm *vm, t_process *ps)
 {
-	int				r1;
-	int				r2;
-	int				r3;
+	unsigned char	encoded;
+	int				bytes;
+	int				p1;
+	int				p2;
+	int				p3;
 
 	printf("Inside op_and\n");//
-	get_next_bytes(vm, ps, &r1, 1);
-	get_next_bytes(vm, ps, &r2, 1);
-	get_next_bytes(vm, ps, &r3, 1);
-	ps->r[r3] = ps->r[r1] + ps->r[r2];
-	modify_pc(ps, 1);
-	if (!r3)
+	printf("pc_tmp %d\n", ps->pc_tmp);
+	printf("pc     %d\n", ps->pc);
+	calc_bytes(encoded, 1, &bytes);
+	printf("bytes 1 %d\n", bytes);
+	get_next_bytes(vm, ps, &p1, bytes);
+
+	calc_bytes(encoded, 2, &bytes);
+	printf("bytes 2 %d\n", bytes);
+	get_next_bytes(vm, ps, &p2, bytes);
+
+	calc_bytes(encoded, 3, &bytes);
+	printf("bytes 3 %d\n", bytes);
+	get_next_bytes(vm, ps, &p3, bytes);
+
+	ps->r[p3] = ps->r[p1] + ps->r[p2];
+	if (!p3)
 		ps->carry = 1;
 	ps->carry = 1;
-	modify_pc(ps, 8);
+	iterate_pc(ps);
+	printf("Leaving op_and\n");//
 }
