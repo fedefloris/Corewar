36043bb90fbfadc65cfa2f0e89511ac054a4ca24
Dav HOJT <davhojt@gmail.com>
2018-06-14 21:50:36 +0200

    Amended modifyy_pc to use get_address.

--- virtual_machine/srcs/modify_pc.c
+++ virtual_machine/srcs/modify_pc.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/12 18:26:09 by dhojt             #+#    #+#             */
-/*   Updated: 2018/06/12 19:19:21 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/06/14 21:49:26 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,5 @@
 
 void				modify_pc(t_process *ps, int modify)
 {
-	ps->pc = ps->pc + modify;
-	if (ps->pc > 0)
-		ps->pc %= MEM_SIZE;
-	else if (ps->pc < 0)
-		ps->pc = (ps->pc % MEM_SIZE) + MEM_SIZE;
+	get_address(ps, modify, &ps->pc);
 }
