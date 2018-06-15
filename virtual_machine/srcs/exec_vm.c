bcbefd19e1921aad35e2a6d409fb61cefae7861b
Dav HOJT <davhojt@gmail.com>
2018-06-15 02:38:54 +0200

    Modified speed of sleep from 1 second to 0.1 seconds.

--- virtual_machine/srcs/exec_vm.c
+++ virtual_machine/srcs/exec_vm.c
@@ -6,7 +6,7 @@
 /*   By: ffloris <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/10 16:52:52 by ffloris           #+#    #+#             */
-/*   Updated: 2018/06/10 16:52:56 by ffloris          ###   ########.fr       */
+/*   Updated: 2018/06/15 01:14:43 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ void		exec_vm(t_vm *vm)
 		if (vm->live_calls > 0 && vm->live_calls % NBR_LIVE == 0 &&
 			(vm->cycle_to_die -= CYCLE_DELTA) <= 0)
 			break ;
-		sleep(1);
+		usleep(100000);
 		// If CYCLE_TO_DIE wasn’t decreased since MAX_CHECKS checkups, decrease it.
 	}
 	// The winner is the last player reported to be “alive”
