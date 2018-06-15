350e657e5d52e2e33f21b7858b5c7fbbc10148ed
Dav HOJT <davhojt@gmail.com>
2018-06-15 13:57:48 +0200

    Added modulo by IDX_MOD.

--- virtual_machine/srcs/operations/op_ld.c
+++ virtual_machine/srcs/operations/op_ld.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/06 23:02:54 by dhojt             #+#    #+#             */
-/*   Updated: 2018/06/15 13:28:53 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/06/15 13:33:26 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void				op_ld(t_vm *vm, t_process *ps)
 	get_next_bytes(vm, ps, &p1, bytes);
 	get_value(vm, ps, decode_byte(encoded, 1), &p1);
 	get_next_bytes(vm, ps, &p2, 1);
-	ps->r[p2] = p1;
+	ps->r[p2] = p1 % IDX_MOD;
 	modify_carry(ps, ps->r[p2]);
 	iterate_pc(ps);
 }
