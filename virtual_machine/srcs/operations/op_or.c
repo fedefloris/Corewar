b44c3a7d88c4c0470fcb1aa08d521ad90ab860dd
Dav HOJT <davhojt@gmail.com>
2018-06-15 03:20:15 +0200

    Created op_or and op_xor from and

--- virtual_machine/srcs/operations/op_or.c
+++ virtual_machine/srcs/operations/op_or.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/06 23:03:48 by dhojt             #+#    #+#             */
-/*   Updated: 2018/06/06 23:03:57 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/06/15 03:17:39 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,24 @@
 
 void				op_or(t_vm *vm, t_process *ps)
 {
-	printf("Inside op_or\n");
+	unsigned char	encoded;
+	int				bytes;
+	int				p1;
+	int				p2;
+	int				p3;
+
+	printf("Inside op_or\n");//
+	modify_pc_tmp(ps, 1);
+	encoded = vm->memory[ps->pc_tmp];
+	calc_bytes(encoded, 1, &bytes);
+	get_next_bytes(vm, ps, &p1, bytes);
+	get_value(vm, ps, decode_byte(encoded, 1), &p1);
+	calc_bytes(encoded, 2, &bytes);
+	get_next_bytes(vm, ps, &p2, bytes);
+	get_value(vm, ps, decode_byte(encoded, 2), &p1);
+	get_next_bytes(vm, ps, &p3, 1);
+	ps->r[p3] = p1 | p2;
+	if (!ps->r[p3])
+		ps->carry = 1;
+	iterate_pc(ps);
 }
