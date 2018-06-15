021ccd02440795fb4061e43978a25b46d9020259
Dav HOJT <davhojt@gmail.com>
2018-06-15 14:40:22 +0200

    Fixed modify_carry

--- virtual_machine/srcs/op_tools/modify_carry.c
+++ virtual_machine/srcs/op_tools/modify_carry.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/15 13:05:55 by dhojt             #+#    #+#             */
-/*   Updated: 2018/06/15 13:08:37 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/06/15 14:34:04 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,6 @@
 
 void				modify_carry(t_process *ps, int value)
 {
-	if (value)
+	if (!value)
 		ps->carry = 1;
 }
