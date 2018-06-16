889245034afc2ccce8031a8d336b19816f54c229
Dav HOJT <davhojt@gmail.com>
2018-06-16 18:07:58 +0200

    Corrected typo.

--- virtual_machine/includes/virtual_machine.h
+++ virtual_machine/includes/virtual_machine.h
@@ -6,7 +6,7 @@
 /*   By: ffloris <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/27 20:58:52 by ffloris           #+#    #+#             */
-/*   Updated: 2018/06/16 18:02:12 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/06/16 18:06:54 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -129,7 +129,7 @@ void					duplicate_process(t_vm *vm, t_process *ps, t_process **new_ps);
 void					insert_sub_process(t_vm *vm, t_process *new_ps);
 void					remove_dead_processes(t_vm *vm);
 
-void					modify_pc(t_process *process, int modift pully);
+void					modify_pc(t_process *process, int modift);
 void					modify_pc_tmp(t_process *process, int modify);
 void					iterate_pc(t_process *ps);
 void					get_address(t_process *ps, int modify, int *address);
