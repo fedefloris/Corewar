0a6b3883b56c68d83707393d6976c4fdc12f37d3
Anastasios KASERIS <kasseristas@gmail.com>
2018-06-17 16:52:52 +0200

    colored output

--- assembler/srcs/ft_output.c
+++ assembler/srcs/ft_output.c
@@ -6,7 +6,7 @@
 /*   By: akaseris <akaseris@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/10 18:07:57 by mfiguera          #+#    #+#             */
-/*   Updated: 2018/06/17 16:32:31 by akaseris         ###   ########.fr       */
+/*   Updated: 2018/06/17 16:50:41 by akaseris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -125,8 +125,8 @@ void		ft_error_output(t_error *error)
 	}
 	while (error)
 	{
-		ft_printf("Error: %s at line %d\n", error->description, error->line_nb);
-		ft_printf("\t\"%s\"\n\n", error->line);
+		ft_printf("$red$Error$eoc$: %s at line $blue$%d$eoc$\n", error->description, error->line_nb);
+		ft_printf("\t$green$\"$eoc$%s$eoc$$green$\"$eoc$\n\n", error->line);
 		error = error->next;
 	}
 }
