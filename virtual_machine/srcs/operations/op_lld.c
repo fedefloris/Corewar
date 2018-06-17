8ebc2a5e81042cd0958bcdbefa3da185065fa3b5
Dav HOJT <davhojt@gmail.com>
2018-06-17 19:43:11 +0200

    Reversed moment of madness and reset lld to before 'deletion'

--- virtual_machine/srcs/operations/op_lld.c
+++ virtual_machine/srcs/operations/op_lld.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/06 23:03:31 by dhojt             #+#    #+#             */
-/*   Updated: 2018/06/17 06:07:57 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/06/17 19:42:03 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,19 @@
 
 void				op_lld(t_vm *vm, t_process *ps)
 {
-	ft_printf("Inside op_lld\n");
-	// modify_carry(ps, ps->r[p2]);
-	// iterate_pc(ps);
+	unsigned char	encoded;
+	int				bytes;
+	int				p1;
+	int				p2;
+
+	ft_printf("Inside op_lld\n");//
+	modify_pc_tmp(ps, 1);
+	encoded = vm->memory[ps->pc_tmp];
+	calc_bytes(encoded, 1, &bytes);
+	get_next_bytes(vm, ps, &p1, bytes);
+	get_value(vm, ps, decode_byte(encoded, 1), &p1);
+	get_next_bytes(vm, ps, &p2, 1);
+	ps->r[p2] = p1;
+	modify_carry(ps, ps->r[p2]);
+	iterate_pc(ps);
 }
