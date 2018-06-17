ce1a9367b8f761afe5e9c906e1c54b5711ae5d6b
Dav HOJT <davhojt@gmail.com>
2018-06-17 05:58:58 +0200

    Working and normed sti.

--- virtual_machine/srcs/operations/op_sti.c
+++ virtual_machine/srcs/operations/op_sti.c
@@ -6,11 +6,22 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/06 23:04:08 by dhojt             #+#    #+#             */
-/*   Updated: 2018/06/17 02:30:21 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/06/17 05:57:13 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "virtual_machine.h"
+
+void				load_bytes(t_vm *vm, t_process *ps, int p1, int p3)
+{
+	int				value;
+
+	value = ps->r[p1];
+	vm->memory[p3 + 0] = (value >> 24) & 0xFF;
+	vm->memory[p3 + 1] = (value >> 16) & 0xFF;
+	vm->memory[p3 + 2] = (value >> 8) & 0xFF;
+	vm->memory[p3 + 3] = value & 0xFF;
+}
 
 void				op_sti(t_vm *vm, t_process *ps)
 {
@@ -22,35 +33,15 @@ void				op_sti(t_vm *vm, t_process *ps)
 
 	ft_printf("Inside op_sti\n");
 	modify_pc_tmp(ps, 1);
-	ft_printf(GREEN);
-	ft_printf("R3 was %x\n", ps->r[3]);
-	ft_printf(RESET);
 	encoded = vm->memory[ps->pc_tmp];
 	get_next_bytes(vm, ps, &p1, 1);
-	if (decode_byte(encoded, 2) == REG_SIZE)
-	{
-		get_next_bytes(vm, ps, &p2, 1);
-		get_value(vm, ps, REG_SIZE, &p2);
-	}
-	else
-	{
-		get_next_bytes(vm, ps, &p2, 2);
-		get_value(vm, ps, IND_SIZE, &p2);
-	}
-	if (decode_byte(encoded, 2) == REG_SIZE)
-	{
-		get_next_bytes(vm, ps, &p3, 1);
-		get_value(vm, ps, REG_SIZE, &p3);
-	}
-	else
-	{
-		get_next_bytes(vm, ps, &p3, 2);
-		get_value(vm, ps, IND_SIZE, &p3);
-	}
+	bytes = (decode_byte(encoded, 2) == REG_SIZE) ? 1 : 2;
+	get_next_bytes(vm, ps, &p2, bytes);
+	get_value(vm, ps, (bytes == 2) ? 2 : 1, &p2);
+	bytes = (decode_byte(encoded, 3) == REG_SIZE) ? 1 : 2;
+	get_next_bytes(vm, ps, &p3, bytes);
+	get_value(vm, ps, (bytes == 2) ? 2 : 1, &p3);
 	get_address(ps, p2 + p3, &p3);
-	vm->memory[p3] = p1;
+	load_bytes(vm, ps, p1, p3);
 	iterate_pc(ps);
-	ft_printf(YELLOW);
-	ft_printf("R3 is %x\n", ps->r[3]);
-	ft_printf(RESET);
 }
