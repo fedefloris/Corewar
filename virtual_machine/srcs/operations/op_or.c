714bed8829a610e216bb51ad9c156583e8760b70
Dav HOJT <davhojt@gmail.com>
2018-06-18 17:26:05 +0200

    Fixed variable typo.

--- virtual_machine/srcs/operations/op_or.c
+++ virtual_machine/srcs/operations/op_or.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/06 23:03:48 by dhojt             #+#    #+#             */
-/*   Updated: 2018/06/15 13:25:05 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/06/18 17:24:56 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ void				op_or(t_vm *vm, t_process *ps)
 	get_value(vm, ps, decode_byte(encoded, 1), &p1);
 	calc_bytes(encoded, 2, &bytes);
 	get_next_bytes(vm, ps, &p2, bytes);
-	get_value(vm, ps, decode_byte(encoded, 2), &p1);
+	get_value(vm, ps, decode_byte(encoded, 2), &p2);
 	get_next_bytes(vm, ps, &p3, 1);
 	ps->r[p3] = p1 | p2;
 	modify_carry(ps, ps->r[p3]);
