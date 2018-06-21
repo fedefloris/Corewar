c1c68865914f0aef0e01908bfe3f4da6102fb7bb
Dav HOJT <davhojt@gmail.com>
2018-06-21 23:18:01 +0200

    Amended 'CLEAR' to 'RESET'

--- virtual_machine/srcs/control/dump_memory.c
+++ virtual_machine/srcs/control/dump_memory.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/19 21:05:02 by dhojt             #+#    #+#             */
-/*   Updated: 2018/06/21 22:51:09 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/06/21 23:05:37 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,7 +84,7 @@ static void			print_cycle_info(t_vm *vm)
 				(ps->id == 3) ? ft_printf(B_CYAN) : 0;
 				(ps->id == 4) ? ft_printf(B_YELLOW) : 0;
 				ft_printf("Player %d (%s)\n", ps->number, ps->name);
-				ft_printf(CLEAR);
+				ft_printf(RESET);
 					break ;
 			}
 			ps = ps->next;
