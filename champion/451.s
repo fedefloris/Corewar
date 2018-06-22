4da36f566f75dbba79c2eacd2e26b647dfd4642b
Dav HOJT <davhojt@gmail.com>
2018-06-22 19:01:02 +0200

    404 Champions not found (except ours).

--- champion/451.s
+++ champion/451.s
@@ -1,25 +1,9 @@
 .name "451"
 .comment "Unavailable For Legal Reasons"
 
-start:	sti r1, %:live, %1
-		sti r1, %:live, %6
-		sti r1, %:live, %11
-		sti r1, %:live, %16
-		sti r1, %:live, %21
-		sti r1, %:live, %26
-		sti r1, %:live, %31
-		sti r1, %:live, %36
-		sti r1, %:live, %41
-		sti r1, %:live, %46
+		aff r1
+		sti r1, %:legal, %1
+		and r1, %0, r1
 
-live:	live %451
-		live %451
-		live %451
-		live %451
-		live %451
-		live %451
-		live %451
-		live %451
-		live %451
-		live %451
-		zjmp %:live
+legal:	live %1
+		zjmp %:legal
