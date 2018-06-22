d3251eaa5237f34106bb994f2a8b578855e924bf
Dav HOJT <davhojt@gmail.com>
2018-06-22 18:15:14 +0200

    Amended Macro to REG_CODE.

--- virtual_machine/srcs/operations/op_sti.c
+++ virtual_machine/srcs/operations/op_sti.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/06 23:04:08 by dhojt             #+#    #+#             */
-/*   Updated: 2018/06/19 15:39:50 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/06/22 17:23:56 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,10 +35,10 @@ void				op_sti(t_vm *vm, t_process *ps)
 	modify_pc_tmp(ps, 1);
 	encoded = vm->memory[ps->pc_tmp];
 	get_next_bytes(vm, ps, &p1, 1);
-	bytes = (decode_byte(encoded, 2) == REG_SIZE) ? 1 : 2;
+	bytes = (decode_byte(encoded, 2) == REG_CODE) ? 1 : 2;
 	get_next_bytes(vm, ps, &p2, bytes);
 	get_value(vm, ps, (bytes == 2) ? 2 : 1, &p2);
-	bytes = (decode_byte(encoded, 3) == REG_SIZE) ? 1 : 2;
+	bytes = (decode_byte(encoded, 3) == REG_CODE) ? 1 : 2;
 	get_next_bytes(vm, ps, &p3, bytes);
 	get_value(vm, ps, (bytes == 2) ? 2 : 1, &p3);
 	load(vm, ps, p1, p2 + p3);
